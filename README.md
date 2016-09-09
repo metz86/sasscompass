d9656864eeb8e93ed9201d834179b94a337603eb
Mads <mads.are.martinsen@gmail.com>
2016-09-09 13:02:04 +0200

    Added susy, created readme

---
+++ README.md
@@ -0,0 +1,3 @@
+#SASS-COMPASS
+
+Sass-compass responsive single-page design. Lynda.com tutorial.
--- config.rb
+++ config.rb
@@ -1,3 +1,4 @@
+require "susy"
 css_dir = 'builds/css'
 sass_dir = 'components/sass'
 javascripts_dir = 'builds/js'
